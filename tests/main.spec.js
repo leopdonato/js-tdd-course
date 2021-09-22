ccd0c8d9305169bd9a993e5f8c0b4f70ba03023d
Leonardo Donato <leo_donato007@yahoo.com.br>
2021-09-22 15:49:52 -0300

    Teste com calculadora simples e inclusão do babel

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,80 +1,65 @@
-var expect = require('chai').expect;
+import { expect } from 'chai';
+import { sum, sub, mult, div } from '../src/main';
 
-describe('Main', function () {
-  // describe('Method A', function() {
-  //   context('Case 1', function () {
-  //     it('should happen test', function() {
-  //       // resultado esperado
-  //       //Entrada de dados / método sum(2,2)
-  //       //Espera retornar (4) => true | (3) => false => broken test
-  //       throw new Error('just an error')
-  //     })
-  //   });
+// var expect = require('chai').expect;
+// var calc = require('../src/main.js');
 
-  //   context.only('Case 2', function () {
-  //     it('should happen test', function() {
-  //       // resultado esperado
-  //       //Entrada de dados / método sum(2,2)
-  //       //Espera retornar (4) => true | (3) => false => broken test
-  //     })
+describe('Calc', () => {
 
-  //     it.skip('should happen test 2', function() {
-  //       // resultado esperado
-  //       //Entrada de dados / método sum(2,2)
-  //       //Espera retornar (4) => true | (3) => false => broken test
-  //     })
-  //   })
-  // });
+  // smoke tests
+  describe('Smoke tests', () => {
 
-  // describe('Method B', function() {
+    it('should exist the method `sum`', () => {
+      expect(sum).to.exist;
+      expect(sum).to.be.a('function');
+    })
 
-  // });
+    it('should exist the method `sub`', () => {
+      expect(sub).to.exist;
+      expect(sub).to.be.a('function');
+    })
 
-  var arr;
+    it('should exist the method `mult`', () => {
+      expect(mult).to.exist;
+      expect(mult).to.be.a('function');
+    })
 
-  // roda uma vez, antes do bloco
-  before(function () {
-    //inicia conexao com banco
-    // criar um conjunto de dados
-  })
-
-  //roda uma vez, depois do bloco
-  after(function () {
-    // fecha conexão com banco
-    // apagar o conjunto de dados
-  })
-
-  //roda todas as vezes vez, antes de CADA bloco
-  beforeEach(function () {
-    arr = [1, 2, 3];
-  })
-
-  //roda todas as vezes vez, depois de CADA bloco
-  afterEach(function () {
+    it('should exist the method `div`', () => {
+      expect(div).to.exist;
+      expect(div).to.be.a('function');
+    })
 
   })
 
-  // testar tipos ou se existe (smoke test)
-  it('should be an array', function () {
-    expect(arr).to.be.an('array');
+  describe('Sum', () => {
+    it('should return 4 when `sum(2,2)`', function() {
+      expect(sum(2,2)).to.be.equal(4);
+    })
   })
 
-  it('should have a size of 4 when push another value to the array', function () {
-    arr.push(4);
-    expect(arr).to.have.lengthOf(4);
+  describe('Sum', () => {
+    it('should return 4 when `sub(6,2)`', function() {
+      expect(sub(6,2)).to.be.equal(4);
+    })
+
+    it('should return -4 when `sub(6,10)`', function() {
+      expect(sub(6,10)).to.be.equal(-4);
+    })
   })
 
-  it('should have a size of 2 when pop a value from the array', function () {
-    arr.pop();
-    expect(arr).to.have.lengthOf(2);
+  describe('Mult', () => {
+    it('should return 4 when `mult(2,2)`', function() {
+      expect(mult(2,2)).to.be.equal(4);
+    })
   })
 
-  it ('should return true when pop 3 from the array', function () {
-    expect(arr.pop() === 3).to.be.true;
-  })
+  describe('Div', () => {
+    it('should return 2 when `div(4,2)`', function() {
+      expect(div(4,2)).to.be.equal(2);
+    })
 
-  it('should remove the value 3 when use pop in the array', function () {
-    arr.pop();
-    expect(arr).to.not.include(3)
+    it('should return `não é possível divisão por zero!` when divide by 0', function() {
+      expect(div(4,0)).to.be.equal('não é possível divisão por zero!');
+    })
   })
 });
